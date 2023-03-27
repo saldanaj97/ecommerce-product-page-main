5fd94824eb44c9c3044dc3fb1cdf07018712149a
Juan Saldana <saldanaj97@gmail.com>
2023-03-27 14:06:04 -0500

    Cart icon states completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,102 @@ import React, { useState } from "react";
 import Head from "next/head";
 
 export default function Home() {
+  const [cartCount, setCartCount] = useState(0);
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [quantity, setQuantity] = useState(0);
+
+  const [item1, setItem1] = useState({
+    id: 1,
+    name: "Fall Limited Edition Sneakers",
+    price: 125.0,
+    preview: "../images/image-product-1.jpg",
+    quantity: 0,
+  });
+
+  // Handle cart open/close
+  const handleCartOpen = () => {
+    setCartOpen(!cartOpen);
+  };
+
+  // Handle quantity increment or decrement
+  const handleQuantityUpdate = (value) => {
+    if (quantity + value >= 0) setQuantity(quantity + value);
+  };
+
+  // Handle adding or removing items from the cart
+  const handleCartItems = (item, count) => {
+    if (count > 0) {
+      setCartItems([...cartItems, item]);
+    } else {
+      setCartItems(cartItems.filter((cartItem) => cartItem !== item));
+    }
+
+    setItem1({ ...item1, quantity: item1.quantity + count });
+    setQuantity(0);
+    setCartCount(cartCount + count);
+  };
+
+  // Render the cart contents if there are any, otherwise render the empty cart message
+  const renderCart = () => {
+    if (cartCount > 0) {
+      return (
+        <div
+          className='user-cart'
+          onMouseLeave={() => {
+            handleCartOpen();
+          }}
+        >
+          <div className='user-cart-contents'>
+            <p className='cart-title'>Cart</p>
+            <hr className='cart-title-break' />
+            <div className='cart-item'>
+              <img className='cart-item-image' src={item1.preview} alt='cart item' />
+              <div className='cart-item-details'>
+                <p className='cart-item-name'>{item1.name}</p>
+                <div className='cart-item-total-pricing'>
+                  <p className='cart-item-price'>${item1.price.toFixed(2)} </p>
+                  <p className='cart-item-quantity'> &nbsp;x {item1.quantity}&nbsp;</p>
+                  <p className='cart-item-total'> ${(item1.price * item1.quantity).toFixed(2)}</p>
+                </div>
+              </div>
+              <div className='cart-item-removal-icon'>
+                <button className='trash-icon' onClick={() => handleCartItems(item1, -item1.quantity)}>
+                  <img src='../images/icon-delete.svg' alt='trash' />
+                </button>
+              </div>
+            </div>
+            <div className='checkout-button'>
+              <button>
+                <p>Checkout</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div
+        className='user-cart'
+        onMouseLeave={() => {
+          handleCartOpen();
+        }}
+      >
+        <div className='user-cart-contents'>
+          <p className='cart-title'>Cart</p>
+          <hr className='cart-title-break' />
+          <div className='empty-cart'>Your cart is empty. </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <>
       <Head>
         <title>Fall Sneakers</title>
         <meta name='description' content='Fall limited edition sneakers' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
-        <link href='https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;600&display=swap' rel='stylesheet' />
       </Head>
       <main className='page-container'>
         <div className='navbar'>
@@ -25,15 +112,24 @@ export default function Home() {
             <a className='tab'>Contact</a>
           </div>
           <div className='user-section'>
-            <a className='cart-icon'>
-              <img class src='../images/icon-cart.svg' alt='cart' />
-            </a>
+            <div className='cart-icon'>
+              <button
+                className='cart-button'
+                onClick={() => {
+                  handleCartOpen();
+                }}
+              >
+                <img src='../images/icon-cart.svg' alt='cart' />
+                {cartCount > 0 && <div className='cart-count'>{cartCount}</div>}
+              </button>
+            </div>
             <a className='user-avatar'>
               <img src='../images/image-avatar.png' alt='avatar' />
             </a>
           </div>
         </div>
         <hr />
+        {cartOpen && <div className='render-cart'>{renderCart()}</div>}
         <div className='product-preview'>
           <div className='product-images'>
             <div className='current-preview'>
@@ -62,17 +158,34 @@ export default function Home() {
             <p className='product-original-price'>$250.00</p>
             <div className='purchasing-options'>
               <div className='quantity-selector'>
-                <button className='quantity-update'>
+                <button
+                  className='quantity-update'
+                  onClick={() => {
+                    handleQuantityUpdate(-1);
+                  }}
+                >
                   <p className='decrement'>-</p>
                 </button>
-                <button className='quantity'>0</button>
-                <button className='quantity-update'>
+                <button className='quantity'>{quantity}</button>
+                <button
+                  className='quantity-update'
+                  onClick={() => {
+                    handleQuantityUpdate(1);
+                  }}
+                >
                   <p className='increment'>+</p>
                 </button>
               </div>
               <div className='add-to-cart'>
-                <img className='add-to-cart-icon' src='../images/icon-cart.svg' alt='cart icon' />
-                <p>Add to Cart</p>
+                <button
+                  className='add-to-cart-button'
+                  onClick={() => {
+                    handleCartItems(item1, quantity);
+                  }}
+                >
+                  <img className='add-to-cart-icon' src='../images/icon-cart.svg' alt='cart icon' />
+                  <p>Add to Cart</p>
+                </button>
               </div>
             </div>
           </div>
